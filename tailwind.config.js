93adb8c22ea7953b744ce95c8d773abe14fdc781
dngtnv <21217751+dngtnv@users.noreply.github.com>
2023-10-25 16:26:22 +0700

    Add font Berkshire

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,9 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        berkshire: ['Berkshire Swash'],
+      },
       colors: {
         primary: '#24283b',
         secondary: '#b4f9f8',
@@ -24,6 +27,8 @@ export default {
         shake: 'shake 0.6s ease-out',
         flip: 'flip 0.5s linear',
         jump: 'jump 0.5s ease',
+        slideIn: 'slideIn 0.2s ease',
+        slideOut: 'slideOut 0.2s ease',
       },
       keyframes: {
         // pop: {
@@ -51,6 +56,14 @@ export default {
           '50%': { transform: 'translateY(-10px)' },
           '75%': { transform: 'translateY(2px)' },
           '100%': { transform: 'translateY(0)' },
+        },
+        slideIn: {
+          '0%': { transform: 'translateY(30px)', opacity: 0 },
+          '100%': { transform: 'translateY(0)', opacity: 1 },
+        },
+        slideOut: {
+          '0%': { transform: 'translateY(0)', opacity: 1 },
+          '100%': { transform: 'translateY(30px)', opacity: 0 },
         },
       },
     },
